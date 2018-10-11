308fe6445423aaac4c90743273989580c160ec36
stevencharleshuang <steven.charles.huang@gmail.com>
2018-10-11 12:56:10 -0400

    completed mod E2

---
+++ E2-Understanding-The-Prototype/E2_UnderstandingThePrototype/Starter/app.js
@@ -0,0 +1,27 @@
+let person = {
+  firstname: 'Default',
+  lastname: 'Default',
+  getFullName: function() {
+    return this.firstname + ' ' + this.lastname;
+  } 
+}
+
+let john = {
+  firstname: 'John',
+  lastname: 'Doe'
+}
+
+// Don't do this EVER, demo only!
+// give john object access to person method getFullName()
+john.__proto__ = person;
+console.log(john.getFullName()); // => 'John Doe'
+// JS Engine Prototype chain finds this property in John object so not person{}'s 'Default'
+console.log(john.firstname); // => 'John'
+
+let jane = {
+  firstname: 'Jane'
+}
+
+jane.__proto__ = person;
+console.log(jane.getFullName()); // => 'Jane Default'
+
